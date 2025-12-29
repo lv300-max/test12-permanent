ac76b168d121e885a09a0c639b367f2256d9e111
lv300 <lv300@yahoo.com>
2025-12-29 11:38:50 -0500

    Include netlify.toml in manual deploy zip

--- make-netlify-zip.sh
+++ make-netlify-zip.sh
@@ -7,7 +7,6 @@ ZIP_PATH="$ROOT_DIR/netlify-upload.zip"
 WEB_DIR="$ROOT_DIR/TEST12/web"
 
 rm -f "$ZIP_PATH"
-(cd "$WEB_DIR" && zip -r "$ZIP_PATH" index.html privacy.html submit.html config.json _redirects assets -x "*/.DS_Store" -x ".DS_Store" >/dev/null)
+(cd "$WEB_DIR" && zip -r "$ZIP_PATH" index.html privacy.html submit.html config.json netlify.toml _redirects assets -x "*/.DS_Store" -x ".DS_Store" >/dev/null)
 
 echo "Wrote: $ZIP_PATH"
-
